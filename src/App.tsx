9e6d796fdf1a1531750d7ba3859683d8465f4838
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-28 16:38:31 +0100

    infinitWords component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TopNav from "./component/navfoot/TopNav";
 import IndividualCategory from "./component/Home/category/IndividualCateg";
+import InfinitWords from "./component/Home/infiniteWords/InfiniteWords";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
       <IndividualCategory />
+      <InfinitWords />
     </div>
   );
 }
---
+++ src/component/Home/infiniteWords/InfiniteWords.tsx
@@ -0,0 +1,27 @@
+import "./infinit.css";
+
+const InfinitWords = () => {
+  return (
+    <div className="position-relative marquee-container d-sm-block bg-secondary">
+      <div className="marquee d-flex justify-content-around">
+        <span>
+          ORDER NOW<b>PAY AT THE END OF DECEMBER. SELECT INVOICE AT CHECKOUT</b>
+        </span>
+        <span>
+          ORDER NOW<b>PAY AT THE END OF DECEMBER. SELECT INVOICE AT CHECKOUT</b>
+        </span>
+      </div>
+      <div className="marquee marquee2 d-flex justify-content-around">
+        <span>
+          ORDER NOW
+          <b>PAY AT THE END OF DECEMBER. SELECT INVOICE AT CHECKOUT</b>
+        </span>
+        <span>
+          ORDER NOW
+          <b>PAY AT THE END OF DECEMBER. SELECT INVOICE AT CHECKOUT</b>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default InfinitWords;
