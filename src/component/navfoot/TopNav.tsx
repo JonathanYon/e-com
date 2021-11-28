074f2530589d2210202e3b343c33d9e61a4432a2
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-28 15:27:34 +0100

    responsive modal

--- src/component/navfoot/TopNav.tsx
+++ src/component/navfoot/TopNav.tsx
@@ -1,10 +1,10 @@
 import "./nav-foot.css";
 
-import { Navbar, Modal, ListGroup, FormControl, Button } from "react-bootstrap";
+import { Navbar, Modal, ListGroup, FormControl, Card } from "react-bootstrap";
 import { FaBars } from "react-icons/fa";
 import { FiSearch, FiShoppingCart } from "react-icons/fi";
 import { BiUser } from "react-icons/bi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // interface ComProps {
 //   title: string;
@@ -13,12 +13,23 @@ import { useState } from "react";
 
 const TopNav = () => {
   const [show, setShow] = useState(false);
+  const [card, setCard] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleCardClose = () => setCard(false);
+  const handleCardShow = () => setCard(true);
+  const [width, setWidth] = useState(window.innerWidth); //<824
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
   return (
     <>
-      <div className="modal-container ">
+      <div className="modal-container">
         <Modal show={show} onHide={handleClose} animation={false}>
           <Modal.Body>
             <ListGroup variant="flush">
@@ -35,12 +46,38 @@ const TopNav = () => {
           </Modal.Body>
         </Modal>
       </div>
+      <div className="">
+        <Modal show={card} onHide={handleCardClose} animation={false}>
+          <Modal.Body className="d-flex">
+            <div
+              className="rounded-circle burger close-circle ml-lg-5"
+              onClick={handleCardClose}
+            >
+              <span className="close-x">X</span>
+            </div>
+            <ListGroup variant="flush">
+              <ListGroup.Item action variant="light">
+                <div className="d-flex justify-content-between">
+                  {/* <h2>Hej! 👋</h2> */}
+                  <h5>Login</h5>
+                </div>
+              </ListGroup.Item>
+              <ListGroup.Item action variant="warning">
+                Light
+              </ListGroup.Item>
+            </ListGroup>
+          </Modal.Body>
+        </Modal>
+      </div>
       <Navbar
         bg="light"
         variant="light"
         className="d-flex justify-content-between"
       >
-        <div className="d-flex" onClick={handleShow}>
+        <div
+          className="d-flex"
+          onClick={width < 824 ? handleShow : handleCardShow}
+        >
           <div className="mr-lg-4 rounded-circle burger ml-lg-5">
             <FaBars />
           </div>
